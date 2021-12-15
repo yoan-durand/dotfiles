ecfe56cbc25d9c215f5a68d72ab0e6314fcb1a68
yoan-durand <1396129+yoan-durand@users.noreply.github.com>
2021-12-15 09:39:11 +0100

    Create flatland.sh

---
+++ theme/flatland.sh
@@ -0,0 +1,22 @@
+export COLOR_01="#1d1d19"           # HOST
+export COLOR_02="#f18339"           # SYNTAX_STRING
+export COLOR_03="#9fd364"           # COMMAND
+export COLOR_04="#f4ef6d"           # COMMAND_COLOR2
+export COLOR_05="#5096be"           # PATH
+export COLOR_06="#695abc"           # SYNTAX_VAR
+export COLOR_07="#d63865"           # PROMP
+export COLOR_08="#ffffff"           #
+
+export COLOR_09="#1d1d19"           #
+export COLOR_10="#d22a24"           # COMMAND_ERROR
+export COLOR_11="#a7d42c"           # EXEC
+export COLOR_12="#ff8949"           #
+export COLOR_13="#61b9d0"           # FOLDER
+export COLOR_14="#695abc"           #
+export COLOR_15="#d63865"           #
+export COLOR_16="#ffffff"           #
+
+export BACKGROUND_COLOR="#1d1f21"   # Background Color
+export FOREGROUND_COLOR="#b8dbef"   # Text
+export CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor
+export PROFILE_NAME="Flatland"
