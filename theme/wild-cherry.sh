d6a4ca52ffd86dcda0974cb2ac450abd84408b05
yoan-durand <1396129+yoan-durand@users.noreply.github.com>
2021-12-15 09:40:19 +0100

    Create wild-cherry.sh

---
+++ theme/wild-cherry.sh
@@ -0,0 +1,22 @@
+export COLOR_01="#000507"           # HOST
+export COLOR_02="#d94085"           # SYNTAX_STRING
+export COLOR_03="#2ab250"           # COMMAND
+export COLOR_04="#ffd16f"           # COMMAND_COLOR2
+export COLOR_05="#883cdc"           # PATH
+export COLOR_06="#ececec"           # SYNTAX_VAR
+export COLOR_07="#c1b8b7"           # PROMP
+export COLOR_08="#fff8de"           #
+
+export COLOR_09="#009cc9"           #
+export COLOR_10="#da6bac"           # COMMAND_ERROR
+export COLOR_11="#f4dca5"           # EXEC
+export COLOR_12="#eac066"           #
+export COLOR_13="#308cba"           # FOLDER
+export COLOR_14="#ae636b"           #
+export COLOR_15="#ff919d"           #
+export COLOR_16="#e4838d"           #
+
+export BACKGROUND_COLOR="#1f1726"   # Background Color
+export FOREGROUND_COLOR="#dafaff"   # Text
+export CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor
+export PROFILE_NAME="Wild Cherry"
